5e935f872035ec6432aad9195946b7d9847d6b50
Samaksh Gupta <samakshgupta04@gmail.com>
2022-02-23 16:11:03 +0530

    Final plugins page for not installed plugins

--- src/pluginStore/index.js
+++ src/pluginStore/index.js
@@ -5,6 +5,7 @@ document.addEventListener('DOMContentLoaded', () => {
         complete: (res) => {
             const data = res.responseJSON;
             console.log(data);
+            paintCards(data);
         }
     });
 });
@@ -34,12 +35,12 @@ function paintCards(data) {
 
         const topDiv = document.createElement('div');
         topDiv.classList = 'card-text item-name';
-        const fake = document.createElement('span')
+        const fake = document.createElement('span');
         fake.style.marginLeft = '5px';
 
         const name = document.createElement('label');
-        name.classList = 'card-label bold'
-        name.innerText = d.name;
+        name.classList = 'card-label bold';
+        name.innerText = `${d.name} v${d.ver}`;
 
         const toolTip = document.createElement('div');
         toolTip.classList = 'tooltip help';
@@ -47,7 +48,36 @@ function paintCards(data) {
         material.classList = 'material-icons md-48';
         material.style.fontSize = 'large';
         material.innerText = 'help';
+        const desc = document.createElement('span');
+        desc.classList = 'tooltiptext tooltip-top';
+        desc.innerText = d.desc;
+        toolTip.appendChild(material);
+        toolTip.appendChild(desc);
 
-        
-    })
+        topDiv.appendChild(fake);
+        topDiv.appendChild(name);
+        topDiv.appendChild(toolTip);
+
+        const image = document.createElement('div');
+        image.style.backgroundImage = `url('${d.img}')`;
+        image.className = 'card-image';
+
+        const bottomDiv = document.createElement('div');
+        bottomDiv.classList = 'card-text bottom';
+        const dlCount = document.createElement('label');
+        dlCount.className = 'util';
+        dlCount.innerText = `${d.dls} Downloads`;
+        const dlBtn = document.createElement('button');
+        dlBtn.classList = 'util download';
+        dlBtn.innerText = 'Download';
+
+        bottomDiv.appendChild(dlCount);
+        bottomDiv.appendChild(dlBtn);
+
+        card.appendChild(topDiv);
+        card.appendChild(image);
+        card.appendChild(bottomDiv);
+
+        document.getElementsByClassName('frame-content')[0].appendChild(card);
+    });
 }
