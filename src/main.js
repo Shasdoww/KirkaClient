336a8eb096e8f39990728c023d78f706969a2caa
Samaksh Gupta <samakshgupta04@gmail.com>
2022-02-24 21:09:02 +0530

    Beta version bump

--- src/main.js
+++ src/main.js
@@ -524,7 +524,7 @@ function createSettings() {
 
     setwin.removeMenu();
     setwin.loadFile(path.join(__dirname, 'settings/settings.html'));
-    setwin.webContents.openDevTools();
+    // setwin.webContents.openDevTools();
     // setwin.setResizable(false)
 
     setwin.on('close', () => {
