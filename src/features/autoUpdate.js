557dad5f13ee64183167fa644c51af61e701996a
AwesomeSam9523 <samakshgupta04@gmail.com>
2022-05-06 16:19:46 +0530

    fixes

--- src/features/autoUpdate.js
+++ src/features/autoUpdate.js
@@ -50,10 +50,7 @@ async function autoUpdate(contents, updateData) {
 }
 
 function ensureFile() {
-    if (!fs.existsSync(path.join(__dirname, '../../../app.asar.unpacked')))
-        fs.mkdirSync(path.join(__dirname, '../../../app.asar.unpacked'));
-
-    fs.writeFileSync(path.join(__dirname, '../../../app.asar.unpacked/update.bat'), `set "params=%*"
+    fs.writeFileSync(path.join(app.getPath('userData'), 'update.bat'), `set "params=%*"
     cd /d "%~dp0" && ( if exist "%temp%\\getadmin.vbs" del "%temp%\\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\\getadmin.vbs" && "%temp%\\getadmin.vbs" && exit /B )
     
     ROBOCOPY "%AppData%/KirkaClient/app.asar" "app.asar" /mov
@@ -94,7 +91,7 @@ async function downloadUpdate(contents, updateData) {
                         app.quit();
                     }
                     ensureFile();
-                    const updatePath = path.join(__dirname, '../../../app.asar.unpacked/update.bat');
+                    const updatePath = path.join(app.getPath('userData'), 'update.bat');
                     log.info(`"${updatePath}"`);
                     const ls = exec(`"${updatePath}"`);
 
