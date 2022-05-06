a7bb78158ee1ac076ef54a51c7785090b9d2b3ef
AwesomeSam9523 <samakshgupta04@gmail.com>
2022-05-06 16:42:02 +0530

    wtf

--- src/features/autoUpdate.js
+++ src/features/autoUpdate.js
@@ -51,7 +51,7 @@ async function autoUpdate(contents, updateData) {
 
 function ensureFile() {
     fs.writeFileSync(path.join(app.getPath('userData'), 'update.bat'), `set "params=%*"
-    cd /d "%~dp0" && ( if exist "%temp%\\getadmin.vbs" del "%temp%\\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\\getadmin.vbs" && "%temp%\\getadmin.vbs" && copy "%AppData%\\KirkaClient\\app.asar" "${path.join(__dirname, '../app.asar')}" /B )
+    cd /d "%~dp0" && ( if exist "%temp%\\getadmin.vbs" del "%temp%\\getadmin.vbs" ) && fsutil dirty query %systemdrive% 1>nul 2>nul || (  echo Set UAC = CreateObject^("Shell.Application"^) : UAC.ShellExecute "cmd.exe", "/k cd ""%~sdp0"" && %~s0 %params%", "", "runas", 1 >> "%temp%\\getadmin.vbs" && "%temp%\\getadmin.vbs" && copy "%AppData%\\KirkaClient\\app.asar" "${path.join(__dirname, '../../')}" /B )
     exit`);
 }
 
